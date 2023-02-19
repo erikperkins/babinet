3397e220c05666e5f357d9da1c6a31e40c062724
Erik Perkins <erik.andrew.perkins@gmail.com>
2023-02-19 09:36:40 -0800

    Add basic unit tests

--- api/main.py
+++ api/main.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
   return "Hello, Docker!\n"
+
+@app.route("/echo", methods = ["POST"])
+def echo():
+  body = request.get_json()
+  return jsonify(body)
